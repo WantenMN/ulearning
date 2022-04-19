139058159e9980262b1bd7aa12636bb1de0bfd69
Wanten <WantenMN@gmail.com>
2022-04-19 17:38:50 +0800

    remove click event of .tab-first

--- userscript/query_of_job_rater.js
+++ userscript/query_of_job_rater.js
@@ -29,69 +29,67 @@
     // scripts don't manipulate nodes
     waitFor('div.each-peeritem').then(()=>{
         // scripts may manipulate these nodes
-        document.getElementById("tab-first").onclick = function() {
-            let url, marker = [], position, str, str1 = "", studentId, homeworkId, allCookies, AUTHORIZATION, homeworkDatil, user;
-            marker[0] = "stuDetail/";
-    
-            //get url
-            url = document.URL;
-            console.log("url: " + url);
-    
-            //get studentId
-            position = url.indexOf(marker[0]);
-            str = url.slice(position+marker[0].length)
-            console.log("str: " + str);
-            position = str.indexOf("/");
-            studentId = str.slice(0, position);
-            console.log("studentId: " + studentId);
-    
-            //get homeworkId
-            str = str.slice(position+1);
-            position = str.indexOf("?VNK");
-            homeworkId = str.slice(0, position);
-            console.log("homeworkId: " + homeworkId);
-            
-            //get AUTHORIZATION
-            AUTHORIZATION = "";
-            allCookies = document.cookie.split(";");
-            let coLength = allCookies.length;
-            for(let i = 0; i < coLength; i++){
-                let co = allCookies[i];
-                if(co.indexOf("AUTHORIZATION") != -1){
-                    AUTHORIZATION = co.slice("AUTHORIZATION=".length + 1);
-                }
+        let url, marker = [], position, str, str1 = "", studentId, homeworkId, allCookies, AUTHORIZATION, homeworkDatil, user;
+        marker[0] = "stuDetail/";
+
+        //get url
+        url = document.URL;
+        console.log("url: " + url);
+
+        //get studentId
+        position = url.indexOf(marker[0]);
+        str = url.slice(position+marker[0].length)
+        console.log("str: " + str);
+        position = str.indexOf("/");
+        studentId = str.slice(0, position);
+        console.log("studentId: " + studentId);
+
+        //get homeworkId
+        str = str.slice(position+1);
+        position = str.indexOf("?VNK");
+        homeworkId = str.slice(0, position);
+        console.log("homeworkId: " + homeworkId);
+        
+        //get AUTHORIZATION
+        AUTHORIZATION = "";
+        allCookies = document.cookie.split(";");
+        let coLength = allCookies.length;
+        for(let i = 0; i < coLength; i++){
+            let co = allCookies[i];
+            if(co.indexOf("AUTHORIZATION") != -1){
+                AUTHORIZATION = co.slice("AUTHORIZATION=".length + 1);
             }
-            console.log("AUTHORIZATION: " + AUTHORIZATION);
-    
-            //get peerReviewHomeworkDatil
-            url = " https://homeworkapi.ulearning.cn/stuHomework/peerReviewHomeworkDatil/" + homeworkId + "/" + studentId;
-            let xhttp = new XMLHttpRequest();
-            xhttp.open('GET', url, true)
-            xhttp.setRequestHeader("AUTHORIZATION", AUTHORIZATION);
-            xhttp.onreadystatechange = function() {
-                if(xhttp.readyState == 4 && xhttp.status == 200){
-                    homeworkDatil = xhttp.response;
-                    console.log(homeworkDatil)
-                    homeworkDatil = JSON.parse(homeworkDatil);
-    
-                    let userLength = homeworkDatil.result.length;
-                    const peerList = document.querySelectorAll(".each-peeritem");
-                    console.log("peerList.length: " + peerList.length)
-                    for(let i = 0; i < userLength; i++){
-                        user = homeworkDatil.result[i];
-                        str = '<div style="color:black;padding-bottom:5px;font:1.4em bold;">'+user.name+'</div>';
-                        peerList[i].innerHTML = str + peerList[i].innerHTML;
-                        str1 += i + 1 + ": <br>"
-                        for(const key in user){
-                            str1 += `${key}: ${user[key]}<br>`
-                        }
-                        str1 += "<br><br>"
-                    }
-                    const footer = document.querySelectorAll("#pane-first");
-                    footer[0].innerHTML = footer[0].innerHTML + str1;
-                }
-            }
-            xhttp.send();
         }
+        console.log("AUTHORIZATION: " + AUTHORIZATION);
+
+        //get peerReviewHomeworkDatil
+        url = " https://homeworkapi.ulearning.cn/stuHomework/peerReviewHomeworkDatil/" + homeworkId + "/" + studentId;
+        let xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url, true)
+        xhttp.setRequestHeader("AUTHORIZATION", AUTHORIZATION);
+        xhttp.onreadystatechange = function() {
+            if(xhttp.readyState == 4 && xhttp.status == 200){
+                homeworkDatil = xhttp.response;
+                console.log(homeworkDatil)
+                homeworkDatil = JSON.parse(homeworkDatil);
+
+                let userLength = homeworkDatil.result.length;
+                const peerList = document.querySelectorAll(".each-peeritem");
+                console.log("peerList.length: " + peerList.length)
+                for(let i = 0; i < userLength; i++){
+                    user = homeworkDatil.result[i];
+                    str = '<div style="color:black;padding-bottom:5px;font:1.4em bold;">'+user.name+'</div>';
+                    peerList[i].innerHTML = str + peerList[i].innerHTML;
+                    str1 += i + 1 + ": <br>"
+                    for(const key in user){
+                        str1 += `${key}: ${user[key]}<br>`
+                    }
+                    str1 += "<br><br>"
+                }
+                const footer = document.querySelectorAll("#pane-first");
+                footer[0].innerHTML = footer[0].innerHTML + str1;
+            }
+        }
+        xhttp.send();
     })
 })();
